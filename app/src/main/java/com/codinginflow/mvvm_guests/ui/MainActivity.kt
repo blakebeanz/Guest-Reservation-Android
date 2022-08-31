1e204c4f25678bd6dcc9d712b719a0cab0df1055
Than Banh <banhlevuthan@gmail.com>
2022-08-31 13:30:42 -0400

    apply navigation graph

--- app/src/main/java/com/codinginflow/mvvm_guests/ui/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvm_guests/ui/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.codinginflow.mvvm_guests.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import mvvm_guests.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
-/*
-private fun initViews() {
-
-}*/
